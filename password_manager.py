ff872e50ede109012bb3860769ec6d8ef20556ed
Hyunjin Kim <117606232+icecreamp@users.noreply.github.com>
2023-02-18 13:05:30 -0500

    Finish password manager

--- password_manager.py
+++ password_manager.py
@@ -3,19 +3,47 @@
 # Author: Hyunjin Kim
 # Reference: https://youtu.be/DLn3jOsNRVE
 
-master_pwd = input("What is the master password?: ")
+# Encrypt textsz
+from cryptography.fernet import Fernet
 
+'''
+# create key
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
+
+# load key
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key() 
+fer = Fernet(key)
+
+# function that shows the existing passwords
 def view():
-    pass
+    with open('passwords.txt', 'r') as f:
+        # read all the lines in the text file
+        for line in f.readlines():
+         data = line.rstrip()
+         user, passw = data.split("|")
+         print("User: ", user, "| Password: ",
+              fer.decrypt(passw.encode()).decode())
 
+# function that create the text file for passwords
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
     # with: automatically close the file
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd)
-        
+        # format the text
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
+# view or add passwords or quit
 while True: 
     mode = input(
         "Would you like to add a new password or view existing ones? (view, add), press q to quit: ").lower()
